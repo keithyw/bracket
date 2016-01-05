5d065dd02032766744222e5d0a7fcdf2367c2fde
Keith Watanabe <conark@gmail.com>
2016-01-04 21:04:46 -0800

    getting the js modules installed

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var webpack = require('webpack'),
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    path = require('path'),
+    srcPath = path.join(__dirname, 'fe');
+
+module.exports = {
+    target: 'web',
+    cache: true,
+    entry: {
+        module: path.join(srcPath, 'module.js'),
+        common: ['react', 'react-dom', 'react-router', 'alt']
+    },
+    resolve: {
+        root: srcPath,
+        extensions: ['', '.js'],
+        modulesDirectories: ['node_modules', 'src']
+    },
+    output: {
+        path: path.join(__dirname, 'tmp'),
+        publicPath: '',
+        filename: '[name].js',
+        library: ['Example', '[name]'],
+        pathInfo: true
+    },
+
+    module: {
+        loaders: [{
+            test: /\.js?$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+            query: { presets: ['react','es2015'] }
+        }]
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin('common', 'common.js'),
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: 'fe/index.html'
+        }),
+        new webpack.NoErrorsPlugin()
+    ],
+
+    debug: true,
+    devtool: 'eval-cheap-module-source-map',
+    devServer: {
+        contentBase: './tmp',
+        historyApiFallback: true
+    }
+};
